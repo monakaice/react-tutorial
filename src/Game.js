0ac70a74d823716c7eda60d63f68dff95ee23369
monakaice <monakaice.programing.3791@gmail.com>
2021-09-16 11:10:35 +0900

    状態を保持する機能を追加

--- src/Game.js
+++ src/Game.js
@@ -2,17 +2,12 @@ import React from 'react'
 import './Game.css'
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
-        {this.state.value}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     )
   }
@@ -20,8 +15,26 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
