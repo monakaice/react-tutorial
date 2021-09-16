9ba2ae604bf58bf73af3b88ede1bb90f177f1ef2
monakaice <monakaice.programing.3791@gmail.com>
2021-09-16 16:19:38 +0900

    勝敗判定機能を追加

--- src/Game.js
+++ src/Game.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : '0'
     this.setState({
       squares: squares,
@@ -37,7 +40,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X': '0')
+    const winner = calculateWinner(this.state.squares)
+    let status = `Next player: ${(this.state.xIsNext ? 'X': '0')}`
+    if (winner) {
+      status = `Winner: ${winner}`
+    }
 
     return (
       <div>
@@ -77,4 +84,27 @@ export default class Game extends React.Component {
       </div>
     )
   }
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (const line of lines) {
+    const [a, b, c] = line
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
 }
